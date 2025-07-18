b0902fd1105a396cbf5135dbac8d12dbbc7ecee6
Zac Delventhal <delventhalz@gmail.com>
2025-07-17 23:09:49 -0500

    Add drop support to Draggable
    Although I am just going to using dragging to rearrange my projects,
    I am going to borrow the logic of "drop zones" or droppables from the
    official Drag and Drop API. I will be able to use the "dragenter" events
    to tell when one project is over another.
    
    The implementation here is straightforward enough. Unfortunately I am
    not able to use pointerenter or pointerleave events. They don't bubble
    and are emitted inconsistently. I can get more reliable behavior by just
    continuing to use pointermove and doing the math myself.

--- public/gallery/draggable.js
+++ public/gallery/draggable.js
@@ -1,6 +1,47 @@
 const DEFAULT_OPTIONS = {
   draggable: '[draggable="true"]',
-  dragTrigger: '[draggable="true"]'
+  dragTrigger: '[draggable="true"]',
+  droppable: '[data-droppable="true"]'
+};
+
+const handleEnterLeave = (event, state) => {
+  const droppables = [...state.settings.container.querySelectorAll(state.settings.droppable)];
+
+  const hits = droppables.filter(element => {
+    if (element === state.target || element === state.placeholder) {
+      return false;
+    }
+
+    const location = element.getBoundingClientRect();
+    return event.x > location.x
+      && event.x < location.x + location.width
+      && event.y > location.y
+      && event.y < location.y + location.height;
+  });
+
+  // Sort hits by which is displayed on top
+  hits.reverse().sort((a, b) => {
+    const zStyleA = a.style.zIndex ?? window.getComputedStyle(a).zIndex;
+    const zStyleB = b.style.zIndex ?? window.getComputedStyle(b).zIndex;
+    const zIndexA = isNaN(zStyleA) ? Number.MIN_SAFE_INTEGER : Number(zStyleA);
+    const zIndexB = isNaN(zStyleB) ? Number.MIN_SAFE_INTEGER : Number(zStyleB);
+    return zIndexB - zIndexA;
+  });
+
+  const dropTarget = hits[0] ?? null;
+  if (dropTarget === state.dropTarget) {
+    return;
+  }
+
+  if (state.dropTarget) {
+    state.dropTarget.dispatchEvent(new DragEvent('dragleave', { ...event, bubbles: true }));
+  }
+
+  state.dropTarget = dropTarget;
+
+  if (dropTarget) {
+    dropTarget.dispatchEvent(new DragEvent('dragenter', { ...event, bubbles: true }));
+  }
 };
 
 const handlePointerMove = (event, state) => {
@@ -8,11 +49,21 @@ const handlePointerMove = (event, state) => {
   state.target.style.top = `${event.y - state.offset.y}px`;
   state.target.style.left = `${event.x - state.offset.x}px`;
   state.target.dispatchEvent(new DragEvent('drag', event));
+
+  // Let new coordinates render immediately before running enter/leave logic
+  requestAnimationFrame(() => {
+    handleEnterLeave(event, state);
+  });
 };
 
 const handlePointerUp = (event, state) => {
   event.preventDefault();
   state.restore();
+
+  if (state.dropTarget) {
+    state.dropTarget.dispatchEvent(new DragEvent('drop', event));
+  }
+
   state.target.dispatchEvent(new DragEvent('dragend', event));
 };
 
@@ -49,7 +100,13 @@ const handlePointerDown = (event, settings) => {
   settings.container.append(target);
 
   // Mutable state shared by all handlers
-  const state = { target, offset, settings };
+  const state = {
+    target,
+    dropTarget: null,
+    placeholder,
+    offset,
+    settings
+  };
 
   const onPointerMove = ev => handlePointerMove(ev, state);
   const onPointerUp = ev => handlePointerUp(ev, state);
@@ -75,18 +132,27 @@ const handlePointerDown = (event, settings) => {
  * is pressed, the normal click/touch behavior is overridden and the wrapping
  * draggable element will move with the pointer until it is lifted.
  *
- * Drag gestures will emit the following DOM events:
+ * Dragging will emit the following DOM events on the draggable element:
  *   - "dragstart"
  *   - "drag" (on every pointer movement)
  *   - "dragend"
  *
- * By default a draggable element is any element with the "draggable" property,
- * but this can be customized by passing a CSS selector to the init options.
+ * Additionally, dragging will emit these events on the top "droppable" element
+ * which the drag gesture passes over:
+ *   - "dragenter"
+ *   - "dragleave"
+ *   - "drop" (if released over droppable element)
  *
- * A drag trigger element must:
- *   - be within a draggable element or be the draggable element itself
- *   - have the style touch-action: none
- *   - match a CSS selector (also '[draggable="true"]' by default)
+ * By default, a draggable element is any element with the "draggable" property
+ * set to "true", and can be customized with a CSS selector in the init options.
+ *
+ * By default, a droppable element is any element with the "data-droppable"
+ * property set to "true", and can also be customized with a CSS selector.
+ *
+ * A drag trigger element must satisfy three criteria:
+ *   - It must be within a draggable element or be a draggable element itself
+ *   - It must have the style touch-action: none
+ *   - It must match a CSS selector ('[draggable="true"]' by default)
  */
 export const initDraggable = (container, options = {}) => {
   const settings = { container, ...DEFAULT_OPTIONS, ...options };
